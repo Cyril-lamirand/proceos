3f4a07b69a4f1c880b7348416fc7c6f34e9a5191
MathiasGilles <fb132fce5>
2021-10-22 14:51:07 +0200

    [BACK-END] [ADD] migration file and api plateform

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
