7c4f010ca3acaef7bfc78801ee5f82f50c491e5b
Cyril-lamirand <contact.cyril1@gmail.com>
2021-11-17 10:13:24 +0100

    [BACK-END] [ADD-UPDATE] After login route : Dashboard
    - Add : DashboardController.php
    - Add : dashboard/index.html.twig
    - Update : base.html.twig
    
    Also adding Bootstrap CSS & JS to the Back-End

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+#[Route('/dashboard')]
+class DashboardController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
