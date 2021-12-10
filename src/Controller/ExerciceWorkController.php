f9182d5c78e3c5e61d10be9db535b8f88e024e67
MathiasGilles <fb132fce5>
2021-12-10 11:24:38 +0100

    [UPDATE] path in quizWorkController and ExerciceWorkController

--- src/Controller/ExerciceWorkController.php
+++ src/Controller/ExerciceWorkController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/exercice/work')]
+#[Route('/exercice_work')]
 class ExerciceWorkController extends AbstractController
 {
     #[Route('/', name: 'exercice_work_index', methods: ['GET'])]
--- src/Controller/QuizWorkController.php
+++ src/Controller/QuizWorkController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/quiz/work')]
+#[Route('/quiz_work')]
 class QuizWorkController extends AbstractController
 {
     #[Route('/', name: 'quiz_work_index', methods: ['GET'])]
