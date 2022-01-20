08bba28332228c81ce10acb20bdf2b67ebb13c41
Cyril-lamirand <contact.cyril1@gmail.com>
2022-01-20 22:45:45 +0100

    [FRONT-END] Update clean version

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,37 +1,38 @@
-import React from "react"
+import React, {useState} from "react"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 // Assets
 import './App.css'
 // Components
 import Main from "./components/layouts/Main"
+// Context
+import {UserContext} from "./contexts/UserContext"
 // Pages
 import Homepage from "./pages/homepage/Homepage"
 import Login from "./pages/login/Login"
 import Register from "./pages/register/Register"
-import Dashboard from "./pages/dashboard/Dashboard";
-import CheckAuth from "./security/CheckAuth";
+import Dashboard from "./pages/dashboard/Dashboard"
+import CheckAuth from "./security/CheckAuth"
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-          <Main>
-              <Routes>
-                  <Route exact path="/" element={ <Homepage/> }/>
-                  <Route exact path="/login" element={ <Login/> }/>
-                  <Route exact path="/register" element={ <Register/> }/>
-                  <Route exact path="/dashboard"
-                    element={
-                      <CheckAuth>
-                        <Dashboard/>
-                      </CheckAuth>
-                    }
-                  />
-              </Routes>
-          </Main>
-      </BrowserRouter>
-    </>
-  )
+
+    const [user, setUser] = useState({})
+
+    return (
+        <>
+            <UserContext.Provider values={[user, setUser]}>
+                <BrowserRouter>
+                    <Main>
+                        <Routes>
+                            <Route exact path="/" element={ <Homepage/> }/>
+                            <Route exact path="/login" element={ <Login/> }/>
+                            <Route exact path="/register" element={ <Register/> }/>
+                            <Route exact path="/dashboard" element={<CheckAuth> <Dashboard/> </CheckAuth>}/>
+                        </Routes>
+                    </Main>
+                </BrowserRouter>
+            </UserContext.Provider>
+        </>
+    )
 }
 
 export default App
