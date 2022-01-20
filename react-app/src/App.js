95f7d6ee6d4be5667bc5951d83cf208e63a8f6c9
Cyril-lamirand <contact.cyril1@gmail.com>
2022-01-20 22:29:01 +0100

    [FRONT-END] Add React APP (Fix II)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+// Assets
+import './App.css'
+// Components
+import Main from "./components/layouts/Main"
+// Pages
+import Homepage from "./pages/homepage/Homepage"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
+import Dashboard from "./pages/dashboard/Dashboard";
+import CheckAuth from "./security/CheckAuth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+          <Main>
+              <Routes>
+                  <Route exact path="/" element={ <Homepage/> }/>
+                  <Route exact path="/login" element={ <Login/> }/>
+                  <Route exact path="/register" element={ <Register/> }/>
+                  <Route exact path="/dashboard"
+                    element={
+                      <CheckAuth>
+                        <Dashboard/>
+                      </CheckAuth>
+                    }
+                  />
+              </Routes>
+          </Main>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
