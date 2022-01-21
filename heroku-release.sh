700dbae375ce86f3f7bdce6f53e68ec9ed8f1087
MathiasGilles <fb132fce5>
2022-01-21 13:09:13 +0100

    [BACK-END][Update] Heroku files

--- heroku-release.sh
+++ heroku-release.sh
@@ -2,4 +2,5 @@
 
 echo "php bin/console doctrine:migrations:migrate"
 php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console doctrine:fixtues:load
 php bin/console cache:clear
