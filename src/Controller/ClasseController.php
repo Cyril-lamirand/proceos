3083b5645979a524c5a6c8b90f06b74865577f2b
MathiasGilles <fb132fce5>
2022-03-18 14:43:42 +0100

    [ADD] admin can add student into classes

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Classe;
+use App\Entity\User;
 use App\Form\ClasseType;
 use App\Repository\ClasseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,7 +24,7 @@ class ClasseController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'classe_new', methods: ['GET','POST'])]
+    #[Route('/new', name: 'classe_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
     {
         $classe = new Classe();
@@ -50,7 +53,7 @@ class ClasseController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'classe_edit', methods: ['GET','POST'])]
+    #[Route('/{id}/edit', name: 'classe_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Classe $classe): Response
     {
         $form = $this->createForm(ClasseType::class, $classe);
@@ -71,12 +74,33 @@ class ClasseController extends AbstractController
     #[Route('/{id}', name: 'classe_delete', methods: ['POST'])]
     public function delete(Request $request, Classe $classe): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$classe->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $classe->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($classe);
             $entityManager->flush();
         }
 
         return $this->redirectToRoute('classe_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/add/student/{id}', name: 'add_student')]
+    public function addStudent(Classe $classe): RedirectResponse
+    {
+        if ($_POST) {
+            $email = $_POST['student'];
+            $student = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $email]);
+            if ($student) {
+                $classe->addUser($student);
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($classe);
+                $manager->flush();
+
+                return $this->redirectToRoute('classe_show', ['id' => $classe->getId()]);
+            }
+
+            throw new Exception("Any student with this email");
+        }
+
+        throw new Exception("Not method POST");
     }
 }
