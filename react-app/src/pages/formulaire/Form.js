4c9221d7dbd85609a46a7ed720404ae969274c3e
victorgbn <victor.gaubin@gmail.com>
2022-11-18 14:57:30 +0100

    create object to post

--- react-app/src/pages/formulaire/Form.js
+++ react-app/src/pages/formulaire/Form.js
@@ -22,42 +22,52 @@ export default function From(){
     let i = 0;
     function addQuestionBloc() {
         i+=1
-        const question_div = `<div className="form-group mt-3">
+        const question_div = `<div className="form-group">
         <label>Question ${i}:</label>
-        <input type='text' placeholder="Votre question" className="form-control question ${i}"></input>
+        <input type='text' placeholder="Votre question" className="form-control" id="question${i}"></input>
         <label>Réponse ${i}:</label>
-        <input type='text' placeholder="Votre réponse" className="form-control reponse${i}"></input>
+        <input type='text' placeholder="Votre réponse" className="form-control" id="reponse${i}"></input>
         </div>`
         let el = document.getElementsByClassName('new_question')
         let createDiv = document.createElement('div')
+        createDiv.classList.add('mb-3')
         createDiv.innerHTML = question_div
         el[0].appendChild(createDiv)
     }
 
-    function sendForm() {
-        console.log("form send")
-        //faire le post 
-    }
+    function sendForm(){
+        let module = document.getElementsByClassName('module') //id
+        let moduleName = document.getElementsByClassName('moduleName') //module name
+        let nbFrom = document.querySelectorAll('.new_question > div') //nb of question
 
-    function test(){
-        let module = document.getElementsByClassName('module')
-        console.log(module[0].value) //id
-        
-        let nbFrom = document.querySelectorAll('.new_question > div')
-        console.log(nbFrom.length) //nb of question
-
-        const result = {
-            label : '',
-                module : module[0].value,
-                questions : []
+        let questions = [] //table questions list
+        for (let x = 1; x <= nbFrom.length; x++){
+            let question = document.getElementById(`question${x}`).value
+            let reponse = document.getElementById(`reponse${x}`).value
+            questions.push(
+                {
+                    'question' : question,
+                    'réponse' : reponse
+                }
+            )
         }
+        const result = {
+            label : moduleName[0].value,
+            module : module[0].value,
+            questions
+        }
+
+        console.log(result)
     }
+
     return(
         <>
         <div>
             <form className="form-group" onSubmit={sendForm}>
+                <label className="mt-3">Nom du quizz</label>
+                <input className="form-control mb-3 mt-3 moduleName" placeholder="Nom du quizz"></input>
                 <label>Choix du module</label>
-                <select className="form-control mb-3 module" name="module">
+                <select className="form-control mb-3 mt-3 module" name="module">
                 <option defaultValue>Veuillez choisir...</option>
                     {modules ?
                         <>
@@ -74,7 +84,7 @@ export default function From(){
                 <div className="new_question"></div>
                 <button className="btn btn-secondary m-3" onClick={addQuestionBloc} type="button">Ajouter une question</button>
                 <button className="btn btn-success m-3" type="submit" onClick={sendForm}>Valider le formulaire</button>
-                <button onClick={test} type="button">Générer l'objet dans la console</button>
+                {/* <button onClick={test} type="button">Générer l'objet dans la console</button> */}
             </form>
         </div>
         </>
