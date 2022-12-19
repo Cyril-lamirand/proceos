0de8a3e3d8c2f2aaaeeee5ce7dae725fc89785ac
MathiasGilles <mathiasgilles136@gmail.com>
2022-12-19 11:27:06 +0100

    [CI/CD] add github actions

--- heroku-release.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "php bin/console doctrine:migrations:migrate"
-php bin/console doctrine:migrations:migrate --no-interaction
-php bin/console doctrine:fixtures:load --append
-php bin/console cache:clear
