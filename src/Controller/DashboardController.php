e82ec767de0ac9c0de7267113d071c533ce727cc
Cyril <59066441+Cyril-lamirand@users.noreply.github.com>
2023-02-26 13:24:02 +0100

    [TEMPLATES] Login & Register (done) / Homepage (progress) (#36)
    * [TEMPLATES & CONTROLLER] Register / Login & Homepage twig
    
    * [TEMPLATES] Login & Register page complete
    
    * [TEMPLATES] Homepage (not finish)

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,13 +3,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Module;
+use App\Entity\User;
+use App\Repository\ModuleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/',name: 'dashboard', methods: ['GET'])]
+    private EntityManagerInterface $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+    /**
+     * This is the main function of this Controller
+     * the objective is to return a dashboard by roles
+     */
+    #[Route('/dashboard',name: 'dashboard', methods: ['GET'])]
     public function index(): Response
     {
         // TODO : Check if Null / Empty
@@ -20,7 +34,12 @@ class DashboardController extends AbstractController
         } elseif (in_array("ROLE_ORGA_ADMIN", $roles)) {
             return $this->render('server/dashboard/index.html.twig');
         } elseif (in_array("ROLE_INTERVENANT", $roles)) {
-            return $this->render('client/teacher/dashboard.html.twig');
+            $modules = $this->manager->getRepository(Module::class);
+            $userModules = $modules->findBy(["user" => $this->getUser()]);
+            dd($userModules);
+            return $this->render('client/teacher/dashboard.html.twig',[
+                "module" => $user->getModules()
+            ]);
         } else {
             return $this->render('client/student/dashboard.html.twig');
         }
@@ -28,4 +47,5 @@ class DashboardController extends AbstractController
             'controller_name' => 'DashboardController',
         ]);
     }
+
 }
