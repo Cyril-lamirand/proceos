5a79db5bab7d077eb37a7c7d2013b10d60490815
Cyril-lamirand <contact.cyril1@gmail.com>
2023-03-01 05:35:13 +0100

    [ADD] Webpack Encore

---
+++ assets/client.js
@@ -0,0 +1,7 @@
+import './styles/client.css';
+// start the Stimulus application
+import './bootstrap';
+// images
+import StudentImg from './images/img-student.jpg';
+import TeacherImg from './images/img-teacher.jpg';
+import OrgaImg from './images/img-orga.jpg';
