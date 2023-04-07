16bb0c18c255eb2a67a15c674e50a802ab2065d1
MathiasGilles <mathiasgilles136@gmail.com>
2023-04-07 11:59:12 +0200

    add route for answer quiz

--- src/Controller/Client/QuizController.php
+++ src/Controller/Client/QuizController.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace App\Controller\Client;
 
 use App\Entity\Module;
+use App\Entity\Quiz;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class QuizController extends AbstractController
@@ -17,12 +17,22 @@ class QuizController extends AbstractController
     }
 
     #[Route(
-        path: '/quizz/create/{id}',
-        name: 'create_quizz'
+        path: '/quiz/create/{id}',
+        name: 'create_quiz',
+        methods: ['POST', 'GET']
     )]
-    public function create(Module $module, Request $request)
+    public function create(Module $module): Response
     {
-
         return $this->render('client/teacher/create-quiz.html.twig');
+    }
+
+    #[Route(
+        path: '/quiz/answer/{id}',
+        name: 'answer_quiz',
+        methods: ['POST', 'GET']
+    )]
+    public function answer(Quiz $quiz): Response
+    {
+        return $this->render('client/student/answer-quiz.html.twig', compact('quiz'));
     }
 }
