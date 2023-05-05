669e397532f71d6df9cf80130da79c84dbce96c7
Cyril-lamirand <contact.cyril1@gmail.com>
2023-05-05 14:10:26 +0200

    Fixtures (Module 2 / Courses)

--- src/DataFixtures/ClasseFixtures.php
+++ src/DataFixtures/ClasseFixtures.php
@@ -16,17 +16,25 @@ class ClasseFixtures extends Fixture implements DependentFixtureInterface
         $students = $manager->getRepository(User::class)->findByRole("ROLE_STUDENT");
         $orga = $manager->getRepository(Organization::class)->findOneBy(['label' => "IIM"]);
         $intervenant = $manager->getRepository(User::class)->findOneBy(['email' => "alexis.bougy@edu.devinci.fr"]);
-
+        // Classe 1
         $classe = new Classe();
         $classe->setLabel("4eme année DW")
             ->setOrganization($orga)
             ->addUser($intervenant);
-
         foreach ($students as $student) {
             $classe->addUser($student);
         }
-
         $manager->persist($classe);
+        // Classe 2
+        $classe2 = new Classe();
+        $classe2->setLabel("5eme année DW")
+            ->setOrganization($orga)
+            ->addUser($intervenant);
+        foreach ($students as $student) {
+            $classe->addUser($student);
+        }
+        $manager->persist($classe2);
+
         $manager->flush();
     }
 
--- src/DataFixtures/CoursesFixtures.php
+++ src/DataFixtures/CoursesFixtures.php
@@ -13,15 +13,47 @@ class CoursesFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
+        // Module 1
         $module = $manager->getRepository(Module::class)->findAll();
-
-        $course = new Course();
-        $course->setTitle("Test cours")
+        $course1 = new Course();
+        $course1->setTitle("Symfony 6 - Débutant")
         ->setCreatedat(new DateTime('NOW'))
-        ->setContent("Description du cours test")
+        ->setContent("Voici le cours concernant symfony 6 niveau débutant !")
         ->setModule($module[0]);
+        $manager->persist($course1);
+        $course2 = new Course();
+        $course2->setTitle("Symfony 6 - Intermédiaire")
+            ->setCreatedat(new DateTime('NOW'))
+            ->setContent("Voici le cours concernant symfony 6 niveau intermédiaire !")
+            ->setModule($module[0]);
+        $manager->persist($course2);
+        $course3 = new Course();
+        $course3->setTitle("Symfony 6 - Avancé")
+            ->setCreatedat(new DateTime('NOW'))
+            ->setContent("Voici le cours concernant symfony 6 niveau avancé !")
+            ->setModule($module[0]);
+        $manager->persist($course3);
 
-        $manager->persist($course);
+        // Module 2
+        $course4 = new Course();
+        $course4->setTitle("Python - Débutant")
+            ->setCreatedat(new DateTime('NOW'))
+            ->setContent("Voici le cours concernant python niveau débutant !")
+            ->setModule($module[1]);
+        $manager->persist($course4);
+        $course5 = new Course();
+        $course5->setTitle("Python - Intermédiaire")
+            ->setCreatedat(new DateTime('NOW'))
+            ->setContent("Voici le cours concernant python niveau intermédiaire !")
+            ->setModule($module[1]);
+        $manager->persist($course5);
+        $course6 = new Course();
+        $course6->setTitle("Python - Avancé")
+            ->setCreatedat(new DateTime('NOW'))
+            ->setContent("Voici le cours concernant python niveau avancé !")
+            ->setModule($module[1]);
+        $manager->persist($course6);
+
         $manager->flush();
     }
 
